10b1ed67d95b7bd2b9d3d35bb7e41de9db708176
NahsJ90 <nashj6@live.it>
2023-01-05 21:09:57 +0100

    adding the Rand1() function

--- U1_main.f95
+++ U1_main.f95
@@ -29,12 +29,42 @@ PROGRAM U1_lattice
             sig_reduced = sig_reduced + (y(i) - data_avg)*(y(i) - data_avg)
         END DO
         sig=DSQRT((REAL(N_data-1,8)/REAL(N_data,8))*sig_reduced)
-        IF (sig0.lt.sig) THEN
-            sig0=sig
+        IF (sig0 .lt. sig) THEN
+            sig0 = sig
         END IF
-        error=sig0
-        average=data_avg
+        error = sig0
+        average = data_avg
 
     END SUBROUTINE jackknife
+
+!--------RAND1----------------------------------------------------------------------------------
+! This is the Rand1 random number generator taken from "Numerical Recipies in Fortran 90, Vol 2"
+
+    FUNCTION rand1(idum)
+
+        IMPLICIT NONE
+        INTEGER, PARAMETER :: K4B = selected_int_kind(9)
+        INTEGER(K4B), INTENT(INOUT) :: idum
+        REAL :: rand1
+        INTEGER(K4B), PARAMETER :: IA=16807, IM=2147483647, IQ=127773, IR=2836
+        REAL, SAVE :: am
+        INTEGER(K4B), SAVE :: ix = -1, iy = -1, k
+        IF (idum <= 0 .or. iy < 0) THEN
+            am = nearest(1.0,-1.0)/IM
+            iy = ior(ieor(888889999,abs(idum)),1)
+            ix = ieor(777755555,abs(idum))
+            idum = abs(idum) + 1
+        END IF
+        ix = ieor(ix,ishft(ix,13)) 
+        ix = ieor(ix,ishft(ix,-17))
+        ix = ieor(ix,ishft(ix,5))
+        k = iy/IQ 
+        iy = IA * (iy-k*IQ) - IR * k
+        IF (iy < 0) THEN 
+            iy = iy + IM
+        END IF
+        rand1 = am * ior(iand(IM,ieor(ix,iy)),1)
+
+    END FUNCTION rand1
 
 END PROGRAM U1_lattice
